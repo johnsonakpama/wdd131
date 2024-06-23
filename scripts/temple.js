8ca8f5836d7cad2b041062856628d1bf7492b90f
johnsonakpama <johnsonakpama33@gmail.com>
2024-06-23 17:40:25 +0100

    Added another javaScripts file

---
+++ scripts/temple.js
@@ -0,0 +1,24 @@
+const msinnav = document.querySelector('.nax');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+const full = document.querySelector("#full");
+const year = document.querySelector("#year");
+const month = document.querySelector("#month");
+const day = document.querySelector("#day");
+
+const today = new Date();
+
+full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "full"
+	}
+).format(today)}</span>`;
+
+
+
